7fdaabf0d85692e6ad4cbeea435d124bcbf4f076
davidflast <davidflasterstein@wustl.edu>
2016-08-07 00:58:14 -0400

    Person Class begining
    Added ability to generate ethnicity and party.

---
+++ Elections/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Elections
+{
+	public class Person
+	{
+		//Fields
+		//Identity Fields
+		public string ethnicity;
+		public string party;
+		// Opinion position and importance
+		public float imm;
+		public float immImport;
+		public float abort;
+		public float abortImport;
+		// Perception of Democrat
+		public float immD;
+		public float immDImport;
+		public float abortD;
+		public float abortDImport;
+		//Perception of Repulican
+		public float immR;
+		public float immRImport;
+		public float abortR;
+		public float abortRImport;
+		// Personality Desire
+		public float care;
+		public float careD;
+		public float careR;
+
+
+		public Person ()
+		{
+			ethnicity = PersonGenerator.generateEthnicity ();
+			party = PersonGenerator.generateParty (ethnicity);
+			
+		}
+	}
+}
+
---
+++ Elections/PersonGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Elections
+{
+	public static class PersonGenerator
+	{
+		public static string generateEthnicity ()
+		{
+			Random r = new Random ();
+			string ethnicity;
+			int e = r.Next (1,100);
+			if (e <= 17) {
+				ethnicity = "black";
+			} else if (e <= 42) {
+				ethnicity = "hispanic";
+			} else {
+				ethnicity = "white";
+			}
+			return ethnicity;
+	
+		}
+
+		public static string generateParty (string e)
+		{
+			string party;
+			Random r = new Random ();
+			int p = r.Next (1, 100);
+			if (e == "black") {
+				if (p <= 82) {
+					party = "dem";
+				} else if(p <= 86){
+					party = "rep";
+				} else {
+					party = "ind";
+				}
+			} else if (e == "hispanic") {
+				if (p <= 39) {
+					party = "dem";
+				} else if(p <= 69){
+					party = "rep";
+				} else {
+					party = "ind";
+				}
+			} else {
+				if (p <= 33) {
+					party = "dem";
+				} else if(p <= 77){
+					party = "rep";
+				} else {
+					party = "ind";
+				}
+			}
+			return party;
+		}
+
+	}
+}
+
